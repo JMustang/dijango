867865e3607509205876d6a9673390450cac0f33
Junior <eecfredes@gmail.com>
2025-12-14 16:44:47 -0300

    :rocket:urls update

--- tutorialProject/todos/urls.py
+++ tutorialProject/todos/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path("temp2", views.temp_view, name="temp_view"),
     path("todos", views.todos_view, name="todos"),
     path("person/<int:person_id>", views.person_details, name="person"),
+    path("delete_todo/<int:todo_id>", views.delete_todo, name="delete_todo"),
+    path("toggle_todo/<int:todo_id>", views.toggle_todo_done, name="toggle_todo"),
     path("path/<str:name>", views.hello_path, name="path_html"),
 ]
