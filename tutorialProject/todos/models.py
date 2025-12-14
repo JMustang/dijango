8aa55ba08d84058b6e002f17893934d94d924b30
Junior <eecfredes@gmail.com>
2025-12-14 15:10:58 -0300

    :rocket:Criando migration para classe Person

--- tutorialProject/todos/models.py
+++ tutorialProject/todos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=100)
-    age = models.ImageField()
+    age = models.IntegerField()
 
     def __str__(self) -> str:
         return f"{self.id} - {self.name}"
